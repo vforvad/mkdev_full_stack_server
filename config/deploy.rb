1fe324f22d76c4b0844b182406ee4dba18917d31
Vadim Sokoltsov <vforvad@gmail.com>
2016-10-14 13:07:55 +0300

    add rubocop ignore directive

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,8 +14,9 @@ set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.3.0'
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)}
-#{fetch(:rbenv_path)}/bin/rbenv exec"
+# rubocop:disable LineLength
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+# rubocop:enable LineLength
 set :rbenv_roles, :all
 
 set :puma_init_active_record, true
