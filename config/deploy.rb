27b5ed657924df22ac6db5b3d87ff3ec597fd38c
Vadim Sokoltsov <vforvad@gmail.com>
2016-10-14 12:29:35 +0300

    change config

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :repo_url, 'git@github.com:vforvad/mkdev_full_stack_server.git'
 set :branch, 'master'
 set :deploy_to, '/home/deploy/applications/mkdev_full_stack'
 
-set :ssh_options, forward_agent: true, user: 'ec2-user', keys: ["#{ENV['HOME']}/.ssh/basic.pem"]
+set :ssh_options, forward_agent: true, user: 'deploy', keys: ["#{ENV['HOME']}/.ssh/basic.pem"]
 set :log_level, :info
 set :linked_files, %w(config/database.yml config/settings.yml)
 set :linked_dirs, %w(bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/uploads)
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@ role :app, ["deploy@#{ip}"]
 role :web, ["deploy@#{ip}"]
 role :db,  ["deploy@#{ip}"]
 
-server ip, user: 'ec2-user', roles: %w(web app db)
+server ip, user: 'deploy', roles: %w(web app db)
 
 set :stage, 'production'
 set :rails_env, 'production'
